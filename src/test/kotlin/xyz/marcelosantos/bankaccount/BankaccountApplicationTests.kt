8d78d2c5b2e029816b0788b1de6475c436c730c0
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-24 19:38:37 -0300

    iniciando o CRUD

---
+++ src/test/kotlin/xyz/marcelosantos/bankaccount/BankaccountApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.marcelosantos.bankaccount
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankaccountApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
